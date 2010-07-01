1b2bfc483ee95e66e75ee8493a35915cf47a7340
Larry Reaves <larry@yrral.net>
2010-07-01 14:43:11 -0700

    added id field

--- lib/types/person.rb
+++ lib/types/person.rb
@@ -2,6 +2,7 @@ require 'lib/datatype'
 
 class OpenGovPerson < OpenGovDataType
   @fields = {
+    :id => 'Id',
     :the_firstest_name => 'First Name',
 #    'fname' => 'First Name',
     :lname => 'Last Name'
