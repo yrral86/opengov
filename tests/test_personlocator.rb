a6edaa0d874297987277e75d9805cd415a57bf2e
Larry Reaves <larry@yrral.net>
2010-08-03 13:48:55 -0700

    added test_create_destroy_person

--- tests/test_personlocator.rb
+++ tests/test_personlocator.rb
@@ -19,8 +19,7 @@ class OpenGovPersonLocatorTest < OpenGovTestCase
   end
 
   def test_create_person
-    person = {:fname => 'FirstNameForTestUser',
-      :lname => 'LastNameForTestUser'}
+    person = fake_person
     post '/personlocator/person', person
     assert_equal 302, last_response.status
     follow_redirects
@@ -28,5 +27,25 @@ class OpenGovPersonLocatorTest < OpenGovTestCase
     p = person_model.find_by_lname(person[:lname])
     assert_equal "/personlocator/person/#{p.id}", last_request.path
     p.destroy
+  end
+
+  def test_create_destroy_person
+    person = fake_person
+    post '/personlocator/person', person
+    params = person.dup
+    params['_method'] = 'delete'
+    follow_redirects
+    id = last_request.path.split('/')[3]
+    person_model = @ch.get_model("PersonLocator::person")
+    p = person_model.find_by_id(id)
+    assert_equal person[:fname], p.fname
+    post "/personlocator/person/#{id}", params
+    p = person_model.find_by_id(id)
+    assert_equal nil, p
+  end
+
+  def fake_person
+    {:fname => 'FirstNameForTestUser',
+      :lname => 'LastNameForTestUser'}
   end
 end
