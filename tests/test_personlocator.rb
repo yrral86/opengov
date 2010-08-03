984cde7097128a452f9e92466e11ffd4725baae7
Larry Reaves <larry@yrral.net>
2010-08-03 13:33:10 -0700

    added test_create_person

--- tests/test_personlocator.rb
+++ tests/test_personlocator.rb
@@ -11,11 +11,22 @@ class OpenGovPersonLocatorTest < OpenGovTestCase
     super
   end
 
-
   def test_personlist
     get '/personlocator/person'
     doc = Nokogiri::HTML(last_response.body)
     records = @ch.get_model("PersonLocator::person").find(:all).length
     assert_equal records + 1, doc.css('a').length
+  end
+
+  def test_create_person
+    person = {:fname => 'FirstNameForTestUser',
+      :lname => 'LastNameForTestUser'}
+    post '/personlocator/person', person
+    assert_equal 302, last_response.status
+    follow_redirects
+    person_model = @ch.get_model("PersonLocator::person")
+    p = person_model.find_by_lname(person[:lname])
+    assert_equal "/personlocator/person/#{p.id}", last_request.path
+    p.destroy
   end
 end
