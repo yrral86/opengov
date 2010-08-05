785e19fd8f143e1dfd2f22de751d657ffc854a7d
Larry Reaves <larry@yrral.net>
2010-08-05 10:21:39 -0700

    remove mark from UserSession and check from view... we don't get errors now that we aren't trying to save

--- components/authenticator/m/usersession.rb
+++ components/authenticator/m/usersession.rb
@@ -5,18 +5,5 @@ require 'drb'
 dir = File.expand_path(File.dirname(__FILE__))
 
 class UserSession < Authlogic::Session::Base
-  attr_accessor :new_record
-
   include DRbUndumped
-
-  def initialize(*args)
-    super(*args)
-    @new_record = true
-  end
-
-  def save_with_mark
-    @new_record = false
-    save_without_mark
-  end
-  alias_method_chain :save, :mark
 end
