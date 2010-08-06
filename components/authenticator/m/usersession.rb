9126a445e7ade7ff6f3018e1c466012f6050f83c
Larry Reaves <larry@yrral.net>
2010-08-05 18:05:49 -0700

    authlogic_pam hack

--- components/authenticator/m/usersession.rb
+++ components/authenticator/m/usersession.rb
@@ -4,6 +4,24 @@ require 'drb'
 
 dir = File.expand_path(File.dirname(__FILE__))
 
+# for some reason on the server authlogic_pam wasn't updating the persistence token
+# and this fixes it... probably a better way when we have time to research
+module FixAuthlogicPamSaveRecord
+  def self.included(klass)
+    klass.class_eval do
+      alias_method_chain :save_record, :hack
+    end
+  end
+
+
+  def save_record_with_hack(attempted_record = nil)
+    attempted_record.reset_persistence_token if attempted_record && !attempted_record.changed?
+    save_record_without_hack(attempted_record)
+  end
+end
+
+
 class UserSession < Authlogic::Session::Base
   include DRbUndumped
+  include FixAuthlogicPamSaveRecord
 end
