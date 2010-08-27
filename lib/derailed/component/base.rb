04884c0ec838a504444ac4a97767758f6335bdf4
Larry Reaves <larry@yrral.net>
2010-08-27 15:20:18 -0700

    documented Derailed::Component::base

--- lib/derailed/component/base.rb
+++ lib/derailed/component/base.rb
@@ -20,11 +20,19 @@ end
 
 module Derailed
   module Component
+    # = Derailed::Component::Base
+    # This class is the base of all components, and handles
+    # registering/deregistering with the ComponentManager, database
+    # initialization, and access to models.  It also provides basic CRUD
+    # functionality for all models via the Derailed::Component::Crud module
     class Base
       include Authentication
       include Controller
       include Crud
 
+      # initialize sets up the database from the config file, initializes the
+      # list of models, checks for dependencies, and then registers the
+      # component with the ComponentManager
       def initialize(name, models, views, dependencies = [], db = 'default')
         @registered = false;
         db_config = YAML::load(File.open(Config::RootDir + '/db/config.yml'))[db]
@@ -65,6 +73,8 @@ module Derailed
         self
       end
 
+      # add_models adds the models passed to it to the component's list of
+      # available models, indexed by the downcased model name
       def add_models(models)
         models.each do |m|
           m.extend(DRbUndumped)
@@ -72,35 +82,43 @@ module Derailed
         end
       end
 
-      def unregistered
-        @registered = false
-      end
-
+      # model_names returns the list of available model names (which are all
+      # downcased)
       def model_names
         @models.keys
       end
 
+      # model returns the requested model (name is downcased)
       def model(name)
         @models[name]
       end
 
+      # name returns the name of the component as set in initialize
       def name
         @name
       end
 
+      # stop stops the daemon
       def stop
         p 'component manager shutting down, exiting ' + self.class.name.to_s
-        # need to figure out magic to properly kill daemon
+        exit
       end
 
+      # daemonize joins the DRb server thread to the main thread
       def daemonize
         DRb.thread.join
       end
 
+      # routes returns the list of routes this component will handle.
+      # The default (defined here) is simply the downcased name of the component
       def routes
         [@name.downcase]
       end
 
+      # call handles the request.  It always sets up the environment, and
+      # by default it provides CRUD functionality for the models.  To override
+      # call in the component to do something besides CRUD, do a call(env,false)
+      # to setup your environment, then handle the request
       def call(env, model_crud = true)
         setup_env(env)
 
