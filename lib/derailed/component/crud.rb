adb4f6b41881795acad646d87eee6c16ec029e2f
Larry Reaves <larry@yrral.net>
2010-08-27 16:35:53 -0700

    documented Derailed::Component::Crud, cleaned up trailing whitespace in personlocator templates

--- lib/derailed/component/crud.rb
+++ lib/derailed/component/crud.rb
@@ -1,6 +1,20 @@
 module Derailed
   module Component
+    # = Derailed::Component::Crud
+    # This module ptovides functions to handle basic CRUD funtionality for the
+    # various models provided by the component.  All that is required besides
+    # the model is three templates in the component's view directory:
+    # 1. modelname.html.erb: displays the record (passed in the variable called
+    #    object)
+    # 2. modelnamelist.html.erb: displays a list of records (passed as
+    #    'objects')
+    # 3. modelnameform.html.erb: displays a form to create/edit a record
+    #    ('object')... also needs a hidden input named _method with value
+    #    <%= method =>
     module Crud
+      # crud handles determining which model we are working with and then
+      # handing control off the the appropriate function based on what we
+      # are trying to do with that model
       def crud(env)
         model_name = next_path
         id = next_path
@@ -42,6 +56,7 @@ module Derailed
         end
       end
 
+      # clean_params removes any paramaters that are not attributes of the model
       def clean_params(model, params)
         new_params = {}
         attributes = model.new.attributes.keys - model.protected_attributes.to_a
@@ -53,6 +68,7 @@ module Derailed
         new_params
       end
 
+      # create creates a new record for the given model with the given params
       def create(model, request)
         params = clean_params(model, request.params)
         object = model.new(params)
@@ -68,6 +84,7 @@ module Derailed
         end
       end
 
+      # read displays modelname.html.erb for the given record
       def read(model, id)
         object = model.find_by_id(id)
         if object then
@@ -82,6 +99,7 @@ module Derailed
         end
       end
 
+      # render_form renders the form for creation/update
       def render_form(model,id)
         if id then
           object = model.find_by_id(id)
@@ -94,6 +112,7 @@ module Derailed
                                          binding)
       end
 
+      # update updated the given record
       def update(model, request)
         id = request.path.split("/")[3]
         object = model.find_by_id(id)
@@ -116,6 +135,7 @@ module Derailed
         end
       end
 
+      # delete deletes the given record
       def delete(model, id)
         object = model.find_by_id(id)
         if object then
@@ -132,6 +152,10 @@ module Derailed
         end
       end
 
+      # view_file returns the filename of a given view
+      # ==== example:
+      # view_file('modelnamelist') returns
+      # RootDir/components/componentname/v/nodelnamelist.html.erb
       def view_file(name)
         Config::RootDir + '/' +
           'components' + '/' +
@@ -140,10 +164,13 @@ module Derailed
           name + '.html.erb'
       end
 
+      # render renders a template from it's name and a binding
       def render(name, binding)
         View.render_erb_from_file_to_string(view_file('_' + name), binding)
       end
 
+      # error_box returns a string containing an unordered list of model
+      # validation errors
       def error_box(record)
         string = "<ul>"
         record.errors.full_messages.each do |m|
