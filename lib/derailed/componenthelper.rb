d2180c588731d96a2ef363990e62b8258d626278
Larry Reaves <larry@yrral.net>
2010-08-27 14:39:24 -0700

    documented componenthelper and requestrouter

--- lib/derailed/componenthelper.rb
+++ lib/derailed/componenthelper.rb
@@ -1,15 +1,26 @@
 require 'drb'
 
 module Derailed
+  # = Derailed::ComponentHelper
+  # This class provides an interface to the components as well as the
+  # ComponentManager
   class ComponentHelper
+    # initialize creates a DRbObject for the ComponentManager
     def initialize
-      @cm = DRbObject.new nil, Derailed::Socket.get_socket_uri('ComponentManager')
+      @cm = DRbObject.new nil,
+      Derailed::Socket.get_socket_uri('ComponentManager')
     end
 
+    # get_current_session invokes current_session on the Authenticator component
+    # which is a subclass of Derailed::Component::Authenticator.  This method
+    # returns the current authenticated session if the user is logged in and
+    # nil otherwise
     def get_current_session(env)
       get_component('Authenticator').current_session(env)
     end
 
+    # get_routes invokes the available_routes method on the ComponentManager.
+    # If the call fails, an empty hash is returned
     def get_routes
       begin
         @cm.available_routes
@@ -18,15 +29,25 @@ module Derailed
       end
     end
 
+    # get_model returns the model given by name.
+    # Name uses the following format ComponentName::modelname.
+    # Models are DRbUndumped so we really return a DRbObject that references
+    # the model.
     def get_model(name)
       component, model = name.split '::'
       get_component(component).model(model)
     end
 
+    # get_component returns a DRbObject representing the component.
+    # The socket uri is fetched from the ComponentManager by a CamelCase
+    # component name
     def get_component(name)
       DRbObject.new nil, @cm.get_component_socket(name)
     end
 
+    # dependencies_not_satisfied returns a list of unsatisfied dependencies
+    # when given a list of dependencies.  In other words, it removes all
+    # dependencies from the list which are met.
     def dependencies_not_satisfied(deps)
       available = {}
       @cm.available_components.each do |c|
@@ -42,6 +63,7 @@ module Derailed
       not_available
     end
 
+    # cm is an accessor for the ComponentManager
     def cm
       @cm
     end
--- lib/derailed/requestrouter.rb
+++ lib/derailed/requestrouter.rb
@@ -1,7 +1,15 @@
 dir = File.expand_path(File.dirname(__FILE__))
 
 module Derailed
+  # = Deraild::RequestRouter
+  # This class is a Rack application that takes incoming requests and routes
+  # them to the appropriate component.
   class RequestRouter
+    # initialize creates a ComponentHelper to access the components and the
+    # component manager, initializes an empty hash for routing paths to
+    # components, and starts the DRb service that allows the components
+    # to access the session and cookie hashes in the
+    # Derailed::Controller::Controller
     def initialize
       @ch = ComponentHelper.new
       @routes = {}
@@ -9,6 +17,14 @@ module Derailed
       self
     end
 
+    # call handles the incoming request.  First, if we have no routes,
+    # we ask the ComponentManager for them via ComponentHelper.get_routes.
+    # We then choose the component via the first part of the path.
+    # If we have a component to handle that path, we attempt to invoke its call
+    # method, otherwise we return a 404 Not found.  If the invocation of call
+    # on the component throws an error, we empty the routes hash (so the new
+    # list of routes are fetched on the next request... obviously at least one
+    # of the routes we had was invalid). We then return a 404.
     def call(env)
       @routes = @ch.get_routes if @routes.empty?
 
