4240eef85c2f46fa33f7ca7f6a35eb5580b9c766
Larry Reaves <larry@yrral.net>
2010-08-27 16:40:45 -0700

    documented Derailed::Socket

--- lib/derailed/socket.rb
+++ lib/derailed/socket.rb
@@ -1,16 +1,22 @@
 require 'yaml'
 
 module Derailed
+  # = Derailed::Socket
+  # This class provides socket uri's based on the selected environment
   class Socket
+    # self.read_config reads the configuration file to determine the socket
+    # directory
     def self.read_config
       @@dir = YAML::load(File.open("#{Config::RootDir}/config/environments.yml"))[Config::Environment]['socket_dir']
       @@dir = "#{File.expand_path(File.dirname(__FILE__))}/../../#{@@dir}" if @@dir[0] == '.'
     end
 
+    # self.get_socket_uri returns the socket uri for a given name
     def self.get_socket_uri(name)
       "drbunix:#{@@dir}/#{name}.sock"
     end
 
+    # self.dir returns the socket directory
     def self.dir
       @@dir
     end
