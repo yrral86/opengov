95969281dea4ba9345b519b9acdfe77ed9da0824
Larry Reaves <larry@yrral.net>
2010-08-28 08:45:37 -0700

    moved componentmanager class to Derailed::Manager

--- lib/derailed.rb
+++ lib/derailed.rb
@@ -3,6 +3,7 @@ dir = File.expand_path(File.dirname(__FILE__))
 require dir + '/derailed/config'
 
 require dir + '/derailed/componentclient'
+require dir + '/derailed/manager'
 require dir + '/derailed/requestrouter'
 require dir + '/derailed/socket'
 require dir + '/derailed/view'
