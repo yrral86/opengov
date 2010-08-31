72fcc519524f9aac866eced36c044e8dbcc63993
Larry Reaves <larry@yrral.net>
2010-08-31 12:40:09 -0700

    fix test runs on server

--- tests/test_environment.rb
+++ tests/test_environment.rb
@@ -1,21 +1,25 @@
 #!/usr/bin/env ruby
 
-require 'debian'
+#require 'debian'
 require 'test/unit'
 
 class OpenGovEnvironmentTest < Test::Unit::TestCase
   def test_system_packages
-    packages = File.read('requirements/packages').split "\n"
-    packages.each do |p|
-      unless p == "" then
-        package = Debian::Dpkg.status([p]).packages[0]
-        assert package, "System package " + p + " not installed"
-        assert_equal "installed",
-        package.status
-        "System package " + p + " not installed"
-      end
-    end
+    assert false, "can not test system packages with enterprise ruby, make " +
+      "sure you run 'sudo apt-get install `cat requirements/packages`'"
   end
+#  def test_system_packages
+#    packages = File.read('requirements/packages').split "\n"
+#    packages.each do |p|
+#      unless p == "" then
+#        package = Debian::Dpkg.status([p]).packages[0]
+#        assert package, "System package " + p + " not installed"
+#        assert_equal "installed",
+#        package.status
+#        "System package " + p + " not installed"
+#      end
+#    end
+#  end
 
   def test_gem_packages
     gems = File.read('requirements/rubypackages').split "\n"
