ddaee0a6350bfb35ab1b01de088d5197d65ad5b4
Larry Reaves <larry@yrral.net>
2010-08-31 10:40:34 -0700

    reorganized daemon.rb

--- lib/derailed/daemon.rb
+++ lib/derailed/daemon.rb
@@ -19,38 +19,6 @@ module Derailed
       end
     end
 
-    def init_ar(db)
-      conf = YAML::load(File.open(Config::RootDir + '/db/config.yml'))[db]
-      ActiveRecord::Base.establish_connection(conf[Config::Environment])
-    end
-
-    def require_models
-      dir = Config::RootDir + "/components-enabled/#{@name.downcase}"
-      original = class_list
-      require_dir(dir)
-      new = class_list
-      new_modules = new - original
-      @new_models = new_modules.select do |m|
-        m.superclass == Derailed::Component::Model ||
-        m.superclass == Authlogic::Session::Base
-      end
-    end
-
-    def class_list
-      array = []
-      ObjectSpace.each_object(Class) {|m| array << m }
-      array
-    end
-
-    def require_dir(dir)
-      old_dir = Dir.pwd
-      Dir.chdir dir
-      Dir.glob '**/*.rb' do |f|
-        require f unless f == 'init.rb'
-      end
-      Dir.chdir old_dir
-    end
-
     def self.component(name)
       new(name)
     end
@@ -68,6 +36,44 @@ module Derailed
           klass.new(@name, @new_models, [], requirements).daemonize
         end
       end
+    end
+
+    private
+
+    def init_ar(db)
+      conf = YAML::load(File.open(Config::RootDir + '/db/config.yml'))[db]
+      ActiveRecord::Base.establish_connection(conf[Config::Environment])
+    end
+
+    def require_models
+      dir = Config::RootDir + "/components-enabled/#{@name.downcase}"
+      original = class_list
+      require_dir(dir)
+      new = class_list
+      new_modules = new - original
+      @new_models = new_modules.select do |m|
+        subclass?(m, Derailed::Component::Model) ||
+        subclass?(m, Authlogic::Session::Base)
+      end
+    end
+
+    def subclass?(m,klass)
+      c.ancestors.include?(klass)
+    end
+
+    def class_list
+      array = []
+      ObjectSpace.each_object(Class) {|m| array << m }
+      array
+    end
+
+    def require_dir(dir)
+      old_dir = Dir.pwd
+      Dir.chdir dir
+      Dir.glob '**/*.rb' do |f|
+        require f unless f == 'init.rb'
+      end
+      Dir.chdir old_dir
     end
   end
 end
