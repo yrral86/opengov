d6e34a443854f5aacd5423661539dedcb6163537
Larry Reaves <larry@yrral.net>
2010-09-08 07:40:36 -0700

    modify component.rb to handle starting/stoping manager as well

--- component.rb
+++ component.rb
@@ -6,14 +6,30 @@ dir = File.expand_path(File.dirname(__FILE__))
 require dir + '/lib/derailed/daemon'
 
 OptionParser.new do |opts|
+  opts.on('-t', '--test', 'use test environment') do
+    ENV['ENV'] = 'test'
+  end
+  opts.on('-d', '--development', 'use development environment') do
+    ENV['ENV'] = 'development'
+  end
+  opts.on('-p', '--production', 'use production environment') do
+    ENV['ENV'] = 'production'
+  end
+  opts.on('-m', '--manager', 'start/stop Manager') do
+    manager = Derailed::Manager::Interface.new
+    daemon = Derailed::Daemon.manager
+    daemon.daemonize do
+      manager.daemonize
+    end
+  end
   opts.on('-c', '--component COMPONENT',
           'start/stop the specified component') do |c|
     config = YAML::load(File.open(Derailed::Config::RootDir +
                                   "/components-enabled/#{c}/config.yml"))
-    component = Derailed::Daemon.component(config['name'])
+    daemon = Derailed::Daemon.component(config['name'])
     config['class'] ||= 'Base'
     config['class'] = Derailed::Component.const_get(config['class'])
     config['requirements'] ||= []
-    component.daemonize(config['class'],config['requirements'])
+    daemon.daemonize(config['class'],config['requirements'])
   end
 end.parse!
