6fb18d546dddf49f971f226f0dcb5a068fba34ef
Larry Reaves <larry@yrral.net>
2010-09-08 13:28:40 -0700

    initial cucumber work

---
+++ features/authentication.feature
@@ -0,0 +1,5 @@
+Feature: Authentication
+  Scenario: User logs in
+    Given I go to '/login'
+    And I login with 'yrral86' and 'password'
+    Then I should be logged in and redirected to '/home'
---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,11 @@
+Given /^I go to '(\S+)'$/ do |url|
+  pending
+end
+
+Given /^I login with '(\S+)' and '(\S+)'$/ do |username, password|
+  pending
+end
+
+Then /^I should be logged in and redirected to '(\S+)'$/ do |url|
+  pending
+end
