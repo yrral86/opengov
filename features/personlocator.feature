63eb51c23dbadb5de80b7cf16f41dad7597c4b85
Larry Reaves <larry@yrral.net>
2010-09-15 14:07:21 -0400

    added create user feature for personlocator

--- features/personlocator.feature
+++ features/personlocator.feature
@@ -3,7 +3,11 @@ Feature: PersonLocator
   As a developer
   I want to have a component that utiliizes it
 
-  Scenario: View list
+  Scenario: View Person List
     Given there are people records in PersonLocator
     When I go to '/personlocator/person'
     Then there should be one more row than 'PersonLocator::Person' records
+
+  Scenario: Create Person
+    When I create a person 'Testy McTesterson' via PersonLocator
+    Then I am viewing the details of person 'McTesterson' via PersonLocator
--- features/step_definitions/personlocator_steps.rb
+++ features/step_definitions/personlocator_steps.rb
@@ -1,4 +1,4 @@
-When /^I create a person '(.*)' via PersonLocator/ do |name|
+When /^I create a person '(.*)' via PersonLocator$/ do |name|
   first, last = name.split
   visit '/personlocator/person/edit'
   fill_in 'fname', :with => first
@@ -10,4 +10,17 @@ Given /^there are people records in PersonLocator$/ do
   rand(10).times {post '/personlocator/person',
     {:fname => "FirstName#{rand(100)}",
       :lname => "LastName#{rand(100)}"}}
+end
+
+Then /^I am viewing the details of person '(.*)' via PersonLocator$/ do |lname|
+  person = @cc.get_model('PersonLocator::Person').find_by_lname(lname)
+  Then "I am at '/personlocator/person/#{person[:id]}'"
+  And "I should see '#{person[:fname]}'"
+  And "I should see '#{person[:lname]}'"
+  And "the HTML should contain " +
+    "'a[href=\"/personlocator/person/edit/#{person[:id]}\"]'"
+  And "the HTML should contain " +
+    "'a[href=\"/personlocator/person\"]'"
+  And "the HTML should contain 'a[href=\"javascript:delete_object('" +
+    "#{person[:id]}','/personlocator/person')\"]'"
 end
