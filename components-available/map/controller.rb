cdb39a4bf44d5e9f8f5e6225a3b3c83eafa945c9
Larry Reaves <larry@yrral.net>
2010-09-20 18:01:59 -0400

    don't include locations... it throws an error on create

--- components-available/map/controller.rb
+++ components-available/map/controller.rb
@@ -1,7 +1,7 @@
 class MapController < Derailed::Component::Controller
   def index
     user = @component.current_user
-    map = Map.find_or_create_by_user_id(user[:id], :include => :locations)
+    map = Map.find_or_create_by_user_id user[:id]
     render 'map', binding
   end
 end
