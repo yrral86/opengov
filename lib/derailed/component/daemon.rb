75045a09541390d470a8babe9acfd7046a4b7e9d
Larry Reaves <larry@yrral.net>
2010-09-22 19:31:03 -0400

    document and clean up component/daemon.rb

--- lib/derailed/component/daemon.rb
+++ lib/derailed/component/daemon.rb
@@ -5,44 +5,41 @@ module Derailed
     # = Derailed::Component::Daemon
     # This class provides the basic daemon structure for components
     class Daemon
-      # initialize stes the name, type and selects the db
+      # initialize processes the configuration file and initializes
+      # ActiveRecord
       def initialize(name)
-        config = read_component_config(name)
-        @name = config['name']
-        @component_class = Component.const_get(config['class'])
-        @requirements = config['requirements']
-        @db = config['db']
-        @db ||= 'default'
+        @config = read_component_config(name)
         init_ar
         self
       end
 
+      # run sets the process name and runs the component
       def run
-        $0 = "OpenGov#{@name}Component"
-        component_proc.call
+        $0 = "OpenGov#{name}Component"
+        class_object.new(name, requirements).daemonize
       end
 
       private
       # init_ar initialize the ActiveRecord connection
       def init_ar
-        conf = YAML::load(File.open(Config::RootDir + '/db/config.yml'))[@db]
+        conf = YAML::load(File.open(Config::RootDir + '/db/config.yml'))[db]
         ActiveRecord::Base.establish_connection(conf[Config::Environment])
       end
 
-      def component_proc
-        proc do
-          @component_class ||= Derailed::Component::Base
-          @component_class.new(@name, @requirements).daemonize
-        end
-
-      end
-
+      # read_component_config reads the config file and sets some defaults
       def read_component_config(component)
         config = YAML::load(File.open(Config::ComponentDir +
                                       "/#{component}/config.yml"))
-        config['class'] ||= 'Base'
         config['requirements'] ||= []
+        config['db'] ||= 'default'
+        config['class'] ||= 'Base'
+        config['class_object'] = Component.const_get(config['class'])
         config
+      end
+
+      # method_missing provides a convient way to access @config
+      def method_missing(id)
+        @config[id.to_s] || super
       end
     end
   end
