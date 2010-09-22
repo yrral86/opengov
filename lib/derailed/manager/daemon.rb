6d1b5955447c003b1c256816f459f76e4b4535f3
Larry Reaves <larry@yrral.net>
2010-09-22 19:15:42 -0400

    documentation and cleanups

--- lib/derailed/manager/daemon.rb
+++ lib/derailed/manager/daemon.rb
@@ -1,17 +1,40 @@
 module Derailed
   module Manager
+    # = Deraild::Manager::Daemon
+    # This class handles controlling component processes.  As early as possible
+    # in the Manager (to minimize copying unnecessary state), call
+    # Daemon.create_spawner.  This will initialize the thread that spawns
+    # components.  Then, for each component create an instance.  Then, you can
+    # control the component with that instance.
+    # === Example:
+    #  static = Manager::Daemon.new('static')
+    #  static.status
+    #  > Component static not running
+    #  static.start
+    #  > Component static started [pid 12345]
+    #  static.status
+    #  > Component static running [pid 12345]
+    #  static.stop
+    #  > Component static stopped
+    #  static.status
+    #  > Component static not running
     class Daemon
       def initialize(name)
         @name = name
       end
 
+      # start sets @@to_start to the name of the component to start and
+      # wakes up @@spawner, waits for it to finish, and then sets @pid
       def start(async = false)
         # write name to class variable @@to_start, shared with spawner thread
         @@to_start_mutex.synchronize do
           @@to_start = @name
         end
+        # wakeup spawneer, and pass until it sets @@to_start back to nil,
+        # indicating it is done
         @@spawner.wakeup
         Thread.pass while @@to_start
+        # set @pid to @@started, and set @@started back to nil
         @@started_mutex.synchronize do
           @pid = @@started
           @@started = nil
@@ -23,14 +46,19 @@ module Derailed
         "Component #{@name} started [pid #{@pid}]"
       end
 
+      # pid= sets the pid... this is used when the component is run in the
+      # control process via the run command
       def pid=(pid)
         @pid = pid
       end
 
+      # stop kills the process
       def stop
         if @pid
-          Process.kill 'TERM', @pid
-          Process.waitpid(@pid)
+          begin
+            Process.kill 'TERM', @pid
+            Process.waitpid(@pid)
+          end
           @pid = nil
           "Component #{@name} stopped"
         else
@@ -38,16 +66,20 @@ module Derailed
         end
       end
 
+      # restart stops, then starts the component
       def restart
         result = stop
         result += "\n#{start}"
       end
 
+      # status returns a string containing the status of the component
       def status
         "Component #{@name} " +
           (running? ? "running [pid #{@pid}]" : "not running")
       end
 
+      # self.create_spawner creates the spawner thread for spawning
+      # component processes
       def self.create_spawner(manager)
         @@manager = manager
         @@to_start = nil
@@ -80,6 +112,7 @@ module Derailed
       end
 
       private
+      # running? returns true if the component is running, and false otherwise
       def running?
         begin
           if @pid
@@ -90,11 +123,6 @@ module Derailed
         rescue
           false
         end
-      end
-
-      def self.wallit(msg)
-#        `echo '#{msg}' | wall`
-        puts msg
       end
     end
   end
