b1b8a72cb69fc2c4dc3933215aaf6873739981d5
Larry Reaves <larry@yrral.net>
2010-09-28 20:12:03 -0400

    added documentation

--- lib/derailed/api/rackrequest.rb
+++ lib/derailed/api/rackrequest.rb
@@ -1,6 +1,14 @@
 module Derailed
   module API
+    ##
+    # Feature: RackRequest API
+    #   As a RackApp instance
+    #   In order to pass requests to a Manager
+    #   I have to encapsulate env in an API
+    ##
     module RackRequest
+      # env -> standard Rack env variable
+      # RackApp wraps env in this type to pass to Manager
       def env; end
     end
   end
--- lib/derailed/api/rackrequesthandler.rb
+++ lib/derailed/api/rackrequesthandler.rb
@@ -1,6 +1,17 @@
 module Derailed
   module API
+    ##
+    # Feature: RackRequestHandler API
+    #   As a Manager instance
+    #   In order to handle a RackRequest
+    #   I want to authenticate the request
+    #   And generate a :request_key
+    #   And pass the key and request to the component
+    ##
     module RackRequestHandler
+      # intiate_request(RackRequest) -> RequestKey
+      # RequestKey is passed by RackApp to request_response, which returns
+      # the response in standard Rack array format
       def initiate_request; end
     end
   end
