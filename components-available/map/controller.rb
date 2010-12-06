79e670dad5eca6cb61891249b4c7ec2a5b6ee141
Larry Reaves <larry@yrral.net>
2010-12-06 17:34:07 -0500

    initial shot at pushing locations, needs polling functionality so we are not making constant requests

--- components-available/map/controller.rb
+++ components-available/map/controller.rb
@@ -4,4 +4,11 @@ class MapController < Derailed::Component::Controller
     map = Map.find_or_create_by_user_id user[:id]
     render 'map', binding
   end
+
+  def locations
+    user = @component.current_user
+    map = Map.find_or_create_by_user_id user[:id]
+    objects = map.locations
+    render 'locations', binding
+  end
 end
