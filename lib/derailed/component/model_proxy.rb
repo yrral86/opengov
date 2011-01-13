5a6cb690301a1c1c7532d13f02be7dd33a0eae1e
Larry Reaves <larry@yrral.net>
2011-01-13 15:42:29 -0500

    fix model_proxy to refresh proxy before drb times out the object

--- lib/derailed/component/model_proxy.rb
+++ lib/derailed/component/model_proxy.rb
@@ -3,6 +3,7 @@ module Derailed
     class ModelProxy
       def self.initProxy(model)
         @@model = model
+        @@m = Mutex.new
       end
 
       def self.method_missing(id, *args)
@@ -11,7 +12,19 @@ module Derailed
 
       private
       def self.proxy
-        @@proxy ||= Service.get_model @@model
+        @@proxy ||= @@m.synchronize do
+          # The proxy is only good for as long as the DRbTimeout,
+          # so expire it at 90% of that
+          t = Thread.new do
+            sleep 0.9*Config::DRbTimeout
+            @@proxy = nil
+          end
+          # we need the sleep to start right away, othewise it could overrun
+          # the timeout and we would have an invalid @@proxy object
+          Thread.pass until t.status == 'sleep'
+          # bookkeeping done, get the model
+          Service.get_model @@model
+        end
       end
     end
   end
